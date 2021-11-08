3e01417ec9a8ab707db5f8fd445b014f44356202
s-zhou <sufang.zhou@estudiantat.upc.edu>
2021-11-08 19:25:14 +0100

    inicar session con google hecho

--- app/src/main/java/com/example/easystore2/MainActivity.java
+++ app/src/main/java/com/example/easystore2/MainActivity.java
@@ -6,21 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import com.example.easystore2.ui.login.LoginActivity;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.entrada);
+        setContentView(R.layout.activity_main);
         int tiempoEntrda = 2000;
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), LoginRegistroActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ContinueWithActivity.class);
                 startActivity(intent);
                 handler.removeCallbacks(null);
             }
